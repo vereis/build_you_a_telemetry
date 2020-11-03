1c3643f7e06137a924474e243f905313a6f1c481
Chris Bailey <me@cbailey.co.uk>
2020-11-03 16:39:39 +0000

    Add Dialyzer

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Telemetry.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -22,7 +25,8 @@ defmodule Telemetry.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 end
